ad8b6e8c3543ee2a776209dd919e98220cba9140
sumiao <15976593304@163.com>
2019-12-17 17:59:11 +0800

    布局登录页面

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,10 +1,26 @@
 <template>
-
+	<div class="content">
+		<div class="logo">
+			<image src="../../static/img/login/JD.png" mode=""></image>
+		</div>
+		<p class="title">景典账号登录</p>
+		
+	</div>
 </template>
 
 <script>
 </script>
 
 <style>
-
+	.content{
+		margin-top: 60rpx;
+	}
+	.content .logo{
+		margin: 0 auto;
+		width: 188rpx;
+		height: 188rpx;
+	}
+	.content .title{
+		text-align: center;
+	}
 </style>
